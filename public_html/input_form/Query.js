a357f729dbf69e98735ba1fdc1297d492342926c
Dave Shepard (CDH) <shepard.david@gmail.com>
2013-12-06 12:28:29 -0800

    Basic Models

--- public_html/input_form/Query.js
+++ public_html/input_form/Query.js
@@ -1,7 +1,34 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+KFA.InputForm.Query = Backbone.Model.extend({
+    defaults: {
+        // collector
+        collector_gender: '',
+        collector_occupation: '',
+        collector_age: '',
+        collector_languages_spoken: [],
 
+        // consultant
+        consultant_gender: '',
+        consultant_occupation: '',
+        consultant_languages_spoken: [],
+        consultant_city: '',
+        consultant_immigration_status: '',
+
+        // context
+        context_name: '',
+        context_event_type: '',
+        collection_time_of_day: '',
+        collection_date: '',
+        collection_weather: '',
+        collection_language: '',
+        collection_place_type: '',
+        collection_others_present: null, // note that NULL means it is not present
+        collection_method : '',
+        collection_description: '',
+
+        // field data
+        project_title: '',
+        media: '',
+        description: ''
+    }
+});
 

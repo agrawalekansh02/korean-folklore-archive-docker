6b43f53476785f22fd302dce5839453dc5b48732
Dave Shepard (CDH) <shepard.david@gmail.com>
2013-12-06 13:14:53 -0800

    Application shell.

--- public_html/AppRoot.js
+++ public_html/AppRoot.js
@@ -1,5 +1,36 @@
 var KFA = {
+    el: "body",
     InputForm: {},
     Map: {},
-    ResultsList: {}
+    ResultsList: {},
+    AppRoot: Backbone.View.extend({
+        render: function () {
+            var search = new KFA.InputForm.Query();
+            // create search form
+            var searchForm = new KFA.InputForm.Form({
+                model: search
+            });
+            $("#search-wrapper").append(searchForm.render());
+
+            var map = new KFA.Map.Map({
+                el: "#map"
+            });
+            map.render();
+            var resultLayer = new KFA.Map.ResultLayer({
+                model: search
+            });
+            map.addLayer(resultLayer);
+
+            var resultList = new KFA.ResultList.ResultList({
+                model: search
+            });
+            $("#result-list-wrapper").append(resultList.render());
+        }
+    })
 };
+
+
+$(function () {
+    var root = new KFA.AppRoot();
+    root.render();
+});

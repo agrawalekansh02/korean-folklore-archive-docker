086ddfdeb172fbb499074cacc7796c3e9d43ef15
Dave Shepard (CDH) <shepard.david@gmail.com>
2013-12-06 15:05:51 -0800

    Enable multi-selects.
    Had to use old version of MultiSelect from CDLI to do this. Fresh version from GitHub had two bugs: 1. it expected there to be a 'title' element on the select and 2. it kept reporting an error that "400" was not a property of an object.

--- public_html/input_form/Form.js
+++ public_html/input_form/Form.js
@@ -1,8 +1,22 @@
 KFA.InputForm.Form = Backbone.View.extend({
-    template: _.template($("#kfa-input-form-template").html()),
+    template: $("#kfa-input-form-template").html(),
+
+    _addMultiSelects: function ($selectors) {
+        for (var i = 0; i < $selectors.length; i++) {
+            this.$el.find($selectors[i])
+                    .multiselect().multiselect("uncheckAll");
+        }
+    },
+
     render: function () {
-        this.$el.append(this.template());
+        this.$el.append(Mustache.compile(this.template));
         // set multiselects
+//        this.$el.find(".context-event-type").multiselect().multiselect("uncheckAll");
+//        this.$el.find(".context-time-of-day").multiselect();
+        this._addMultiSelects([
+            ".context-event-type", ".context-time-of-day", ".collection-method",
+            '.collection-place-type', '.media'
+        ]);
         // clone form
         // add to page
         return this;
@@ -13,7 +27,7 @@ KFA.InputForm.Form = Backbone.View.extend({
     },
 
     events: {
-        "click.search" : "_updateModel"
+        "submit.search" : "_updateModel"
     }
 });
 

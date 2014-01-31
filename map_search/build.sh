c3349bc3e6a55e00570cedaa2db0c2130e2780ec
Dave Shepard (CDH) <shepard.david@gmail.com>
2014-01-31 08:03:34 -0800

    Add build step to remove unnecessary files.

--- map_search/build.sh
+++ map_search/build.sh
@@ -13,9 +13,27 @@ unzip packages/jQRangeSlider-5.5.0.zip
 mv jQRangeSlider-5.5.0 ..
 rm packages/jQRangeSlider-5.5.0.zip
 cd ..
-# Get rid of demos for smaller upload/build
+# Get rid of original for smaller upload
+rm -rf jQRangeSlider
+# Get rid of demos for smaller upload
 rm -rf jQRangeSlider-5.5.0/demo
 cd ..
+
+# Build OpenLayers
+echo "Building OpenLayers"
+cd bower_components/openlayers/build
+./build.py
+mv OpenLayers.js ..
+rm -rf apidoc_config
+rm -rf build
+rm -rf doc_config
+rm -rf examples
+rm -rf tests
+cd ../../..
+
+# Get rid of some files for smaller upload
+rm -rf bower_components/backbone/docs
+rm -rf bower_components/backbone/examples
 
 
 # QB:

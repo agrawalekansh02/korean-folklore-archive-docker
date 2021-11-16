60f28eb2c402bd9314f573984ac4accd3846edff
Ekansh Agrawal <agrawalekansh@gmail.com>
2021-11-16 14:34:49 -0800

    reconfig of files

--- contents/index.php
+++ contents/index.php
@@ -1,27 +1,57 @@
-<?php 
-$cms->title = 'UCLA Korean Folklore Archive';
-$user = check_auth(); 
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+include_once('lib.php');
+include_once('mini/cms.php');
+
+$cms->js[] = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+$cms->js[] = "https://code.jquery.com/ui/1.12.1/jquery-ui.js";
+$cms->js[] = "js/kfl.js";
+
+$cms->css[] = "//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css";
+$cms->css[] = "css/layout.css";
+$cms->css[] = "css/menu.css";
+
+$content = $cms->content();
+$menu = $cms->content('menu');
+header('Content-Type: text/html; charset=utf-8'); 
 ?>
-<div style="margin-left:520px" class="small-label">
-	<form id="role_form" action="handler/collector/<?php echo $user->get('id');?>/role" method="post">
-	<?php
-	$user = check_auth();
-	if ($user->get('sid')== 'tango'|| $user->get('sid')=='jwan123'){
-		$status_array = array('0'=>"regular user", '1'=>"admin");
-		echo "You login as " . $status_array[$user->get('status')-1];
-		$new_status = !($user->get('status')-1)+1;
-		echo ". <a href='javascript:$(\"#collector_status\").val(".$new_status.");submitform(\"role_form\");'>Click to switch to ".  $status_array[!($user->get('status')-1)]."</a>";
-	}
-	?>
-	<input type="hidden" id="collector_status" name="collector_status" value="">
-	</form>
-</div>
-<div style="margin-top:20px">
-<p class="display-text">
-The Korean / Korean American Online Folklore Archive is a growing on-line archive of contemporary popular and traditional culture of Korea. The archive is based entirely on student collections in two upper division Asian Languages and Cultures courses (Korean M183 and Korean 187) at UCLA, serving between seventy and one hundred undergraduate students per academic year. In each class, a significant  portion of the coursework is comprised of individual, original fieldwork among the populations of Korean heritage in Southern California. Students input their fieldwork through a web interface and the results of their fieldwork is stored in a database residing on a server maintained by the Center for Digital Humanities. Currently, there are over 4000 records in the archive.
-</p>
-<p class="display-text">
-The majority of the folklore collected in the archive is either in English or in Korean. Korean language recordings, and Korean words, are transcribed using the McCune-Reischauer transcription method. Instructions on transcribing according to the McCune-Reischauer system can be found <a href = 'http://roman.cs.pusan.ac.kr/input_eng.aspx' target=_blank>here</a>.
-</p>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+	<?php echo $cms->head(); ?> 
+</head>
+<body>
+<?php
+if ($_SERVER['SERVER_NAME']=='localhost'){
+	echo "<h1>This is localhost site</h1>";
+}
+?>
+<div id="wrapper">
+
+	<div id="topWrapper"></div>
+
+	<table id="mainArea">
+	<tr>
+		<td class="leftcolumn">
+			<?php echo $menu; ?>
+		</td>
+		<td class="rightcolumn">
+			<div id="contentArea">
+				<?php echo $content; ?>
+			</div>
+		</td>
+	</tr>
+	</table>
+
+	<div id="footer">
+		<a href="https://www.universityofcalifornia.edu/">University of California</a> Copyright &copy; <?php echo date("Y"); ?> UC Regents&nbsp;&nbsp;|&nbsp;&nbsp;
+		<a href="https://cdh.ucla.edu/ticket" target="_blank">Web Support</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+		<a href="https://bitbucket.org/uclacdh/kfl-map-search" target="_blank">Open Source Code</a>
+	</div>
 
 </div>
+
+
+</body>
+</html>
